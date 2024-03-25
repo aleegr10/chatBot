6b03a73b417ce6e16c96d742d360b59c2a462231
alee <tailerby10@gmail.com>
2024-03-25 10:23:05 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-apt-get update
-apt-get install -y sqlite3=3.35.0
+
+# Descargar la versión específica de SQLite
+wget https://www.sqlite.org/2022/sqlite-amalgamation-3360000.zip
+unzip sqlite-amalgamation-3360000.zip
+cd sqlite-amalgamation-3360000
+
+# Construir e instalar SQLite
+./configure
+make
+make install
+
+# Volver al directorio principal del proyecto
+cd ..
+
+# Continuar con la ejecución de la aplicación Python
+python3 app.py
